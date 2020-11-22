f835bb879e199cc8d3270d6b7e97a08eabf19016
akuma121 <72017747+akuma121@users.noreply.github.com>
2020-11-22 21:14:39 +0900

    課題
    課題完成

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 15;
+        array[1] = 18;
+        array[2] = 25;
+        array[3] = 14;
+        array[4] = 5;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int j = 4; j >= 0; j--)
+        {
+            Debug.Log(array[j]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+
+    public void Mp(int 残mp)
+    {
+
+        if(mp >= 0)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+            this.mp -= 残mp;
+        }
+
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+
+    }
+}
+public class Test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+
+        lastboss.Defence(3);
+
+     
+        {
+            for(int a = 0; a < 12; a++)
+            {
+                lastboss.Mp(5);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
